21e55c24d4f90a4f5dfe031e4cb05f2fecd7c7aa
Muthuraj Muthulingam <muthurajmuthulingam@gmail.com>
2015-11-30 12:33:08 +0530

    Initial Commit

---
+++ MyMind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMind
+//
+//  Created by Muthuraj Muthulingam on 11/30/15.
+//  Copyright © 2015 Muthuraj Muthulingam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
