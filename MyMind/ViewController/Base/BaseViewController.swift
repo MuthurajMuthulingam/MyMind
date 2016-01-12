35558dd852016f8ff0aa713f8bc40b0a3564d98f
Muthuraj Muthulingam <muthuraj@Muthurajs-MacBook-Pro.local>
2016-01-12 17:45:27 +0530

    Menu Views added / animatons - reg

--- MyMind/ViewController/Base/BaseViewController.swift
+++ MyMind/ViewController/Base/BaseViewController.swift
@@ -13,6 +13,7 @@ class BaseViewController: UIViewController {
     var menuViewController:MenuViewController!
     var dashboardViewController:DashboardViewController!
     var newNavController:UINavigationController!
+    var startXPos:Float!
     
     override func loadView() {
         super.loadView()
@@ -20,6 +21,9 @@ class BaseViewController: UIViewController {
     }
     
     func createUserInterface() {
+        
+        startXPos = 0
+        
         menuViewController = MenuViewController()
         menuViewController.view.frame = self.view.frame
         self.view .addSubview(menuViewController.view)
@@ -31,11 +35,32 @@ class BaseViewController: UIViewController {
         let menuBtn:UIBarButtonItem = UIBarButtonItem(title: "Add", style: UIBarButtonItemStyle.Done, target: self, action: "menuClicked")
         newNavController.navigationItem.leftBarButtonItem = menuBtn
         newNavController.view.frame = self.view.frame
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tapped")
+        newNavController.view.addGestureRecognizer(tap)
         self.view.addSubview(newNavController.view)
         self.addChildViewController(newNavController)
         newNavController.didMoveToParentViewController(self)
         
     }
+    
+    func tapped() {
+        if(startXPos == 0) {
+            startXPos = Float(self.view.frame.size.width*0.8)
+        } else {
+            startXPos = 0
+        }
+        
+        animateMenu()
+    }
+    
+    // Mark: Show/Hide Menu
+    
+    func animateMenu() {
+        UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+            self.newNavController.view.frame = CGRectMake(CGFloat(self.startXPos), 0, self.view.frame.size.width, self.view.frame.size.height)
+            }, completion: nil)
+    }
+    
     func menuClicked() {
         print("Menu Clicked")
     }
