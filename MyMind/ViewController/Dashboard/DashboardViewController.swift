d4f39590b5ad1cc788f15001414a243840117c69
Muthuraj Muthulingam <muthurajmuthulingam@gmail.com>
2016-03-14 12:09:30 +0530

    Dashboard Changes- reg

--- MyMind/ViewController/Dashboard/DashboardViewController.swift
+++ MyMind/ViewController/Dashboard/DashboardViewController.swift
@@ -48,15 +48,4 @@ class DashboardViewController: MainViewController,DashboardViewDelegate {
     func dashboardViewItemClicked(dashboardView: DashboardView?, clickedItem: NSDictionary?) {
         self.mainDelegate?.viewTapped(self)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
